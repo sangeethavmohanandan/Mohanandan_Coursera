c9eb5b2b62e18736aa4b71302987d95d2923ccc0
Sangeetha Mohanandan <sangeethavmohanandan@gmail.com>
2020-10-02 07:11:30 -0600

    Add code outline and documentation for C1M1 assesment

--- stats.c
+++ stats.c
@@ -9,17 +9,16 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file Mohanandan_Coursera
+ * @brief Analyse the array parameters
+ * In this program, an array is given. Print the given array
+ * Then sort the given array in descending order.  
+ * Print the array statistics such as mean, median, maximum and minimum value of the given array. 
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Sangeetha Mohanandan
+ * @date 28/10/2020
  *
  */
-
-
 
 #include <stdio.h>
 #include "stats.h"
@@ -27,17 +26,43 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+void main()
+{
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+  unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+                              114, 88, 45, 76, 123, 87, 25, 23,
+                              200, 122, 150, 90, 92, 87, 177, 244,
+                              201, 6, 12, 60, 8, 2, 5, 67,
+                              7, 87, 250, 230, 99, 3, 100, 90};
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
-
+  printf("Given an array of size 40 shown below\n");
+  print_array(test, SIZE);
+  printf("The sorted array is\n");
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+  print_statistics(test, SIZE);
 }
-
+void print_array(unsigned char *ptr, int count)
+{
+}
+float find_mean(unsigned char *ptr, int count)
+{
+}
+unsigned char find_maximum(unsigned char *ptr, int count)
+{
+}
+unsigned char find_minimum(unsigned char *ptr, int count)
+{
+}
+void sort_array(unsigned char *numbers, int count)
+{
+}
+unsigned char find_median(unsigned char array[], int count)
+{
+}
+void print_statistics(unsigned char ptr[], int count)
+{
+}
 /* Add other Implementation File Code Here */
--- stats.h
+++ stats.h
@@ -9,32 +9,116 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file Mohanandan_Coursera
+ * @brief Analyse the array parameters
+ * In this program, an array is given. Print the given array
+ * Then sort the given array in descending order.  
+ * Print the array statistics such as mean, median, maximum and minimum value of the given array. 
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Sangeetha Mohanandan
+ * @date 28/10/2020
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Add Your Declarations and Function Comments here */
+
+void print_statistics(unsigned char ptr[], int count);
+/**
+ * @brief Print an array.
+ *
+ * This function takes inputs, an unsigned character that points to an array element and 
+ * an integer value denotes the size of the array.
+ * Instead of returning parameter, it will print the elements until the array ends.
+ *
+ * @param *ptr The element of array to be printed
+ * @param count The size of the array to be printed
+ *
+ * @return No return value
+ */
+void print_array(unsigned char *ptr, int count);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Determines the mean of given array.
  *
- * <Add Extended Description Here>
+ * This function takes inputs, an unsigned character that points to an array element and 
+ * an integer value denotes the size of the array.
+ * It will returned mean of the given array.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param *ptr This points to the  element of the array 
+ * @param count The size of the array 
  *
- * @return <Add Return Informaiton here>
+ * @return The mean value of the array.
+ */
+float find_mean(unsigned char *ptr, int count);
+
+/**
+ * @brief Determines the maximum value of given array.
+ *
+ * This function takes inputs, an unsigned character that points to an array element and 
+ * an integer value denotes the size of the array.
+ * It will returned an unsigned character denotes the maximum value of the given array.
+ *
+ * @param *ptr This points to the  element of the array 
+ * @param count The size of the array 
+ *
+ * @return The maximum value of the array.
+ */
+unsigned char find_maximum(unsigned char *ptr, int count);
+
+/**
+ * @brief Determines the minimum value of given array.
+ *
+ * This function takes inputs, an unsigned character that points to an array element and 
+ * an integer value denotes the size of the array.
+ * It will returned an unsigned character denotes the minimum value of the given array.
+ *
+ * @param *ptr This points to the  element of the array 
+ * @param count The size of the array 
+ *
+ * @return The minimum value of the array.
  */
 
+unsigned char find_minimum(unsigned char *ptr, int count);
+/**
+ * @brief Sort the given array in descending order array.
+ *
+ * This function takes inputs, an unsigned character that points to an array element and 
+ * an integer value denotes the size of the array.
+ * Instead of returning any parameter, it will sorts the elements of the array in descending order.
+ * @param *numbers Points to the elements of an array to be sorted
+ * @param count The size of the array to be sorted
+ *
+ * @return No return value
+ */
+void sort_array(unsigned char *numbers, int count);
+/**
+ * @brief Determines the median of given array.
+ *
+ * This function takes inputs, an unsigned character array and 
+ * an integer value denotes the size of the array.
+ * It will returned an unsigned character denotes the median of the given array.
+ *
+ * @param array The given array to find the median 
+ * @param count The size of the array 
+ *
+ * @return The median of the array.
+ */
 
+unsigned char find_median(unsigned char array[], int count);
+
+/**
+ * @brief Print the statistics of an array.
+ *
+ * This function takes inputs, an unsigned character array  and 
+ * an integer value denotes the size of the array.
+ * Instead of returning parameter, it will print the statics of the array including mean, median, maximum and minimum value of the array.
+ *
+ * @param ptr[] The array to print the statistics
+ * @param count The size of the array 
+ *
+ * @return No return value
+ */
+void print_statistics(unsigned char ptr[], int count);
 #endif /* __STATS_H__ */
